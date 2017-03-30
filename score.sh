25b3eeb06c8c95459ea65f3907a8fcd53682d5c7
songzy <songzy.thu@qq.com>
2017-03-30 23:37:38 +0800

    score

--- score.sh
+++ score.sh
@@ -6,7 +6,7 @@ BASEDIR=$(pwd)
 
 usage() {
     echo "put this shell script at the same level of *.zip *.rar files"
-    echo "put all the possible input files in data/1.in, data/1.out, etc."
+    echo "put all the possible input files in data/1.in, data/2.in, etc."
     echo "a score.txt will be generated containing all the scores"
     echo "press any key to continue..."
     read 
@@ -14,6 +14,7 @@ usage() {
 
 input_info() {
     echo
+    echo "0: stdin"
     echo "1.in: "
     echo "2.in: 8 employers"
     echo "3.in: aA1bB2cC3dD4"
@@ -21,28 +22,39 @@ input_info() {
 }
 
 command_info() {
+    echo $filename_
+    echo $ID
     echo
-    echo "press r to run, EOF for break:"
+    echo "press r to run, c for continue:"
 }
 
 compile_and_run() {
     for filename_ in *.c*
     do
         echo $filename_
-        sed -i 's/void main/int main/g' $filename_
-        cat $filename_
+        sed -i 's/void main/int main/g' "$filename_"
+        sed -i 's/scanf_s/scanf/g' "$filename_"
+        cat "$filename_"
         command_info
-        while read command ; do
+        while read -n1 command ; do
             case $command in 
                 r) 
                 echo "run"
-                g++ $filename_
+                g++ "$filename_"
                 input_info
                 read input
-                ./a.out < $BASEDIR/data/$input.in
-                cat $BASEDIR/data/$input.out
+                case $input in
+                    0) ./a.out
+                    ;;
+                    *)
+                    ./a.out < $BASEDIR/data/$input.in
+                    ;;
+                esac
                 ;;
+                c)
+                break
             esac
+            cat "$filename_"
             command_info
         done
     done
@@ -56,19 +68,20 @@ actions() {
         cd $(ls)
     fi
     
+    ID=(${filename//_/ })
+    echo $ID" press any key to continue..."
+    read
     compile_and_run
 
     cd $BASEDIR
-
-    filename="${filename%.*}"
-    ID=(${filename//_/ })
 
     echo "$ID score:"
     read point
     echo "$ID: $point" >> score.txt
 }
 
-usage
+#usage
+rm -r tmp
 echo > score.txt
 
 for filename in *.zip
@@ -76,6 +89,7 @@ do
     unzip -o $filename -d tmp 
     actions
     rm -r tmp
+    rm $filename
 done
 
 for filename in *.rar
@@ -85,6 +99,7 @@ do
     
     actions
     rm -r tmp
+    rm $filename
 done
 
 less score.txt
